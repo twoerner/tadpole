20099b6f08ac1759f6f2c36f7b686389870f2e62
Trevor Woerner <twoerner@gmail.com>
2016-06-06 19:57:22 -0400

    tadpole_blink.sh: add
    Signed-off-by: Trevor Woerner <twoerner@gmail.com>

---
+++ tadpole_blink.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Copyright (c) 2016 Trevor Woerner
+# see LICENSE file in top-level directory
+
+SLEEPTIME_MS=120000
+
+if [ ! -d /sys/class/gpio/gpiochip338 ]; then
+	echo "can't find /sys/class/gpio/gpiochip338, wrong board?"
+	exit 1
+fi
+
+gpio_start() {
+	echo 338 > /sys/class/gpio/export
+	echo 339 > /sys/class/gpio/export
+	echo 340 > /sys/class/gpio/export
+
+	echo out > /sys/class/gpio/gpio338/direction
+	echo out > /sys/class/gpio/gpio339/direction
+	echo out > /sys/class/gpio/gpio340/direction
+
+	echo 1 > /sys/class/gpio/gpio338/value
+	echo 1 > /sys/class/gpio/gpio339/value
+	echo 1 > /sys/class/gpio/gpio340/value
+}
+
+gpio_cleanup() {
+	echo 1 > /sys/class/gpio/gpio338/value
+	echo 1 > /sys/class/gpio/gpio339/value
+	echo 1 > /sys/class/gpio/gpio340/value
+
+	echo 338 > /sys/class/gpio/unexport
+	echo 339 > /sys/class/gpio/unexport
+	echo 340 > /sys/class/gpio/unexport
+}
+
+trap gpio_cleanup EXIT
+
+gpio_cleanup > /dev/null 2>&1
+gpio_start
+while [ 1 ]; do
+	echo 0 > /sys/class/gpio/gpio338/value
+	echo 0 > /sys/class/gpio/gpio339/value
+	echo 0 > /sys/class/gpio/gpio340/value
+	usleep $SLEEPTIME_MS
+
+	echo 0 > /sys/class/gpio/gpio338/value
+	echo 0 > /sys/class/gpio/gpio339/value
+	echo 1 > /sys/class/gpio/gpio340/value
+	usleep $SLEEPTIME_MS
+
+	echo 0 > /sys/class/gpio/gpio338/value
+	echo 1 > /sys/class/gpio/gpio339/value
+	echo 0 > /sys/class/gpio/gpio340/value
+	usleep $SLEEPTIME_MS
+
+	echo 0 > /sys/class/gpio/gpio338/value
+	echo 1 > /sys/class/gpio/gpio339/value
+	echo 1 > /sys/class/gpio/gpio340/value
+	usleep $SLEEPTIME_MS
+
+	echo 1 > /sys/class/gpio/gpio338/value
+	echo 0 > /sys/class/gpio/gpio339/value
+	echo 0 > /sys/class/gpio/gpio340/value
+	usleep $SLEEPTIME_MS
+
+	echo 1 > /sys/class/gpio/gpio338/value
+	echo 0 > /sys/class/gpio/gpio339/value
+	echo 1 > /sys/class/gpio/gpio340/value
+	usleep $SLEEPTIME_MS
+
+	echo 1 > /sys/class/gpio/gpio338/value
+	echo 1 > /sys/class/gpio/gpio339/value
+	echo 0 > /sys/class/gpio/gpio340/value
+	usleep $SLEEPTIME_MS
+done
